eeda02d92ffd5eb4c43aae3b0813ab9fbf5fe13f
PAILLEX <loic.paillex@sogeti.com>
2017-06-01 11:26:36 +0200

    add file for heroku

---
+++ www/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+var oneDay = 86400000;
+
+app.use(express.compress());
+
+app.use(express.static(__dirname, { maxAge: oneDay }));
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.listen(process.env.PORT);
+
+app.post('/', function(req, res){
+  var result = req.rawBody;
+  res.send("hello there world data is " + result);
+});
+
+
